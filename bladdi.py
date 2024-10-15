e28e65983840992f178576f4c9f598ea895b26fe
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2024-10-15 15:23:09 -0700

    Update bladdi.py

--- bladdi.py
+++ bladdi.py
@@ -1,25 +1,28 @@
 import turtle as trtl
+import random
 
-class OpticalSpiral:
-    turtle_shapes = ["square", "circle", "triangle", "classic"]
-    turtle_colors = ["purple", "red", "yellow", "blue"]
-    turtle_dict = {
-        "square": "purple",
-        "circle": "red",
-        "triangle": "yellow",
-        "classic": "blue"
-    }
 
-    def __init__(self, shape, colors, x, y, direction):
-        self.p = trtl.Turtle(shape=shape)
-        self.p.speed(0)
-        self.shape = shape
-        self.colors = colors
-        self.x = x
-        self.y = y
-        self.p.goto(x, y)
-        self.direction = direction
-        self.p.setheading(direction)
+colors = ["white", "red", "pink", "purple", "blue", "yellow", "orange", "black", "green"]
+p = trtl.Turtle()
+p.speed(0)
+p.hideturtle()
+p.pensize(4)
+center_lines = 4
 
-    def main(self):
-        
+def draw_lines(x, y):
+    p.penup()
+    x = 0
+    y = 0
+    p.goto(x, y)
+    p.pendown()
+    for lines in range(center_lines):
+        color = random.choice(colors)
+        p.color(color)
+        p.forward(200)
+        p.backward(200)
+        p.right(60)
+
+draw_lines(0, 0)
+
+wn = trtl.Screen()
+wn.mainloop()
