abc39259b3fae22f528070233a08e7def7ae649d
Aditya-Jayanthi-CS <157422413+Aditya-Jayanthi-CS@users.noreply.github.com>
2024-10-18 15:26:59 -0700

    Update spiderman.py

--- spiderman.py
+++ spiderman.py
@@ -1,63 +1,137 @@
-def draw_spiderman():
-    turn_right(45)
-    up()
-    teleport(-75,-125)
-    down()
-    p.fillcolor("red4")
-    p.begin_fill()
-    for i in range(2):
-        circ(100, 90)
-        circ(200, 90) 
-    turn_left(135)
-    p.end_fill()
+import turtle as trtl
+trtl.bgcolor("black")
+p = trtl.Turtle()
+p.pencolor("white")
+p.speed(0)
 
-    #right_eye
+def turn_left(degree):
+    p.left(degree)
 
-    #outer eye
+def turn_right(degree):
+    p.right(degree)
 
-    up()
-    move(250)
-    turn_left(90)
-    move(25)
-    x,y = p.pos()
-    turn_left(45)
-    down()
+def move(distance):
+    p.forward(distance)
 
-    p.fillcolor("black")
-    p.begin_fill()
+def circ(degree, extent):
+    p.circle(degree, extent)
 
-    circ(75,120)
-    circ(45,60)
-    turn_left(45)
-    up()
+def up():
+    p.penup()
 
-    teleport(x,y)
-    turn_left(60)
-    down()
-    circ(160,-52)
+def down():
+    p.pendown()
 
-    p.end_fill()
+def teleport(x,y):
+    p.goto(x,y)
 
 
-    #inner eye
-    up()
-    teleport(x+12.5,y-25)
-    turn_left(125)
-    down()
-    web()
+turn_right(45)
+up()
+teleport(-75,-125)
+down()
+p.fillcolor("red4")
+p.begin_fill()
+for i in range(2):
+    circ(100,90)
+    circ(200,90) 
+turn_left(135)
+p.end_fill()
 
-    p.fillcolor("white")
-    p.begin_fill()
+#left_eye
 
-    circ(75*5/7,120)
-    circ(45*5/7,60)
-    turn_left(45)
-    up()
-    teleport(x+12.5,y-25)
-    turn_left(60)
-    down()
-    circ(160*5/7,-51.25)
+#outer eye
 
-    p.end_fill()
-    
-    #ends here
+up()
+move(250)
+turn_left(90)
+move(25)
+x,y = p.pos()
+turn_left(45)
+down()
+
+p.fillcolor("black")
+p.begin_fill()
+
+circ(75,120)
+circ(45,60)
+turn_left(45)
+up()
+teleport(x,y)
+turn_left(60)
+down()
+circ(160,-52)
+
+p.end_fill()
+
+
+#inner eye
+up()
+teleport(x+12.5,y-25)
+turn_left(125)
+down()
+
+p.fillcolor("white")
+p.begin_fill()
+
+circ(75*5/7,120)
+circ(45*5/7,60)
+turn_left(45)
+up()
+teleport(x+12.5,y-25)
+turn_left(60)
+down()
+circ(160*5/7,-51.25)
+
+p.end_fill()
+#ends here
+
+
+#right eye
+#outer eye
+up()
+teleport(88,125)
+down()
+turn_left(45)
+x2,y2 = p.pos()
+
+p.fillcolor("black")
+p.begin_fill()
+
+circ(75,-120)
+circ(45,-60)
+turn_left(45)
+
+up()
+teleport(x2,y2)
+turn_right(150)
+down()
+circ(160,52)
+p.end_fill()
+
+#inner eye
+up()
+teleport(x2-12.5,y2-25)
+down()
+
+p.fillcolor("white")
+p.begin_fill()
+
+turn_right(97.5)
+circ(45*5/7,-60)
+circ(75*5/7,-120)
+
+turn_right(45)
+up()
+teleport(x2-12.5,y2-25)
+turn_left(60)
+down()
+turn_right(142.25)
+circ(160*5/7,51.25)
+
+p.end_fill()
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
