dea7236170b116e61eca6aa986e6c9da9b2ffa77
Aditya-Jayanthi-CS <157422413+Aditya-Jayanthi-CS@users.noreply.github.com>
2024-10-21 00:02:24 -0700

    Update spiderman.py

--- spiderman.py
+++ spiderman.py
@@ -1,7 +1,6 @@
 import turtle as trtl
 trtl.bgcolor("black")
 p = trtl.Turtle()
-p.pencolor("white")
 p.speed(0)
 
 def turn_left(degree):
@@ -25,12 +24,12 @@ def down():
 def teleport(x,y):
     p.goto(x,y)
 
-
+#oval face
 turn_right(45)
 up()
 teleport(-75,-125)
 down()
-p.fillcolor("red4")
+p.fillcolor("red3")
 p.begin_fill()
 for i in range(2):
     circ(100,90)
@@ -38,14 +37,56 @@ for i in range(2):
 turn_left(135)
 p.end_fill()
 
-#left_eye
+#web face
+#positioning
+up()
+teleport(0,0)
+down()
 
-#outer eye
+#face straight lines
+p.pencolor("black")
+for i in range(12):
+    p.setheading(-90)
+    up()
+    teleport(0,0)
+    down()
+    turn_left(30*(i-1))
+    move(200)
+
+#web face curved lines
+up()
+teleport(-7.5,15)
+down()
+for i in range(6):
+    p.setheading(-90)
+    turn_right(60*(i-1))
+    circ(15,60)
 
 up()
+teleport(37.5,75)
+down()
+for n in range(3):
+    for i in range(12):
+        p.setheading(-90)
+        turn_right(30*(i-1))
+        circ(75+25*n,30)
+    if n==0:
+        up()
+        teleport(50,100)
+        down()
+    if n==1:
+        up()
+        teleport(62.5,125)
+        down()
+
+#left_eye
+#outer eye
+p.pencolor("white")
+up()
+teleport(-100,-125)
+turn_left(90)
 move(250)
 turn_left(90)
-move(25)
 x,y = p.pos()
 turn_left(45)
 down()
@@ -127,11 +168,9 @@ teleport(x2-12.5,y2-25)
 turn_left(60)
 down()
 turn_right(142.25)
-circ(160*5/7,51.25)
+circ(160*5/7,51.75)
 
 p.end_fill()
-
-
 
 wn = trtl.Screen()
 wn.mainloop()
